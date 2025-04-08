0ff71e35e44ecd968d8d5547005fa58cd4fb46f0
Anthony Metcalf <anthony.metcalf@ajbell.co.uk>
2025-04-08 10:37:30 +0100

    feat: nvim-tree keybind working

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -1,3 +1,18 @@
+local function my_on_attach(bufnr)
+  local api = require 'nvim-tree.api'
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', '<leader>v', api.node.open.vertical)
+  vim.keymap.set('n', '?', api.tree.toggle_help, opts 'Help')
+end
+
 return {
   'nvim-tree/nvim-tree.lua',
   version = '*',
@@ -7,15 +22,10 @@ return {
   },
   config = function()
     require('nvim-tree').setup {
+      on_attach = my_on_attach,
       disable_netrw = true,
       hijack_netrw = true,
-      view = {
-        mappings = {
-          list = {
-            { key = "v", cb = tree_cb "vsplit" },
-          }
-        }
-      }
+      auto_close = true,
     }
     vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { silent = true })
   end,
